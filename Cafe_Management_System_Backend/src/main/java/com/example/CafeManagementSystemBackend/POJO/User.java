c7123d62d4e69da910fe1e42552b5fecbe54c9d1
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-20 16:23:55 +0530

    create the model

---
+++ Cafe_Management_System_Backend/src/main/java/com/example/CafeManagementSystemBackend/POJO/User.java
@@ -0,0 +1,39 @@
+package com.example.CafeManagementSystemBackend.POJO;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data//lambok
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+}
