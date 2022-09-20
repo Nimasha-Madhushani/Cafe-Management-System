0b3cd802d1cd43212ff45a4f5fb4b357fb5499ec
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-20 08:02:37 +0530

    initiate backend application

---
+++ Cafe_Management_System_Backend/src/main/java/com/example/Cafe_Management_System_Backend/CafeManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cafe_Management_System_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ Cafe_Management_System_Backend/src/test/java/com/example/Cafe_Management_System_Backend/CafeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cafe_Management_System_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
