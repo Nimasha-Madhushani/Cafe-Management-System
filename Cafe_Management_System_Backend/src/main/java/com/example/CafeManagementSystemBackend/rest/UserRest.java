420a9000bf1045b9f27a7fcf9b8806fe4b7e17b2
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-21 16:10:10 +0530

    start to create end points

---
+++ Cafe_Management_System_Backend/src/main/java/com/example/CafeManagementSystemBackend/rest/UserRest.java
@@ -0,0 +1,8 @@
+package com.example.CafeManagementSystemBackend.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "/user")
+public interface UserRest {
+
+}
---
+++ Cafe_Management_System_Backend/src/main/java/com/example/CafeManagementSystemBackend/restimpl/UserRestImpl.java
@@ -0,0 +1,9 @@
+package com.example.CafeManagementSystemBackend.restimpl;
+
+import com.example.CafeManagementSystemBackend.rest.UserRest;
+import com.example.CafeManagementSystemBackend.service.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestImpl implements UserRest {
+}
