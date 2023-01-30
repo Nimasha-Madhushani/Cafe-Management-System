639b9759d79b0c1b430c8f896f7c24dc5e1cf2db
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-31 00:38:54 +0530

    initialize the app

--- Cafe_Management_System_Backend/src/main/java/com/example/CafeManagementSystemBackend/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.CafeManagementSystemBackend.service;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface UserService {
-    ResponseEntity<String>signUp(Map<String,String>requestMap);
-}
